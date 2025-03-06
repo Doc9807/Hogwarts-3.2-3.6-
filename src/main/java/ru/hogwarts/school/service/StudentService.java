ffac3c7457a3d1ceb8e6e46c20d03129009837e8
Vladimir Muraito <vl.muraito@mail.ru>
2025-03-07 01:17:26 +1000

    HomeWork3.4 (Version 1).

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -3,6 +3,7 @@ package ru.hogwarts.school.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exception.EntityNotFoundException;
+import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.StudentRepository;
 
@@ -43,11 +44,18 @@ public class StudentService {
         studentRepository.deleteById(id);
     }
 
-    public List<Student> getStudentsByAge(int age) {
-        return studentRepository.findByAge(age);
+    public List<Student> getStudentsByAgeBetween(int minAge, int maxAge) {
+        return studentRepository.findByAgeBetween(minAge, maxAge);
     }
 
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
+    }
+
+    public Faculty getFacultyByStudentId(Long studentId) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() ->
+                new EntityNotFoundException("Student not found with id: " + studentId));
+        return student.getFaculty();
     }
 }
